b60db8c2901b3afa2a90f53213513d56b9f197a0
Mario Mancilla <mariormt17@gmail.com>
2021-04-02 00:40:32 -0600

    User and post tests created

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "validate presence of title" do
+      should validate_presence_of(:title)
+    end
+
+    it "validate presence of content" do
+      should validate_presence_of(:content)
+    end
+
+    it "validate presence of published" do
+      should validate_presence_of(:published)
+    end
+
+    it "validate presence of user_id" do
+      should validate_presence_of(:user_id)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "validate presence of email" do
+      should validate_presence_of(:email)
+    end
+
+    it "validate presence of name" do
+      should validate_presence_of(:name)
+    end
+
+    it "validate presence of auth_token" do
+      should validate_presence_of(:auth_token)
+    end
+
+    it "validate relations" do
+      should have_many(:posts)
+    end
+  end
 end
