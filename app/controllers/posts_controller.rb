dbd3a9a19459efac71774a51479b93fb71b7c6dc
Mario Mancilla <mariormt17@gmail.com>
2021-04-04 16:11:24 -0500

    Create and update methods added in the posts controller and its routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,36 @@
 class PostsController < ApplicationController
-  # GET /post/
+  # GET /posts/
   def index
     @posts = Post.where(published: true)
     render json: @posts, status: :ok
   end
 
-  # GET /post/{id}
+  # GET /posts/{id}
   def show
     @post = Post.find(params[:id])
     render json: @post, status: :ok
+  end
+
+  # POST /posts
+  def create
+    @post = Post.create!(create_params)
+    render json: @post, status: :created
+  end
+
+  # PUT /posts/{id}
+  def update
+    @post = Post.find(params[:id])
+    @post.update!(update_params)
+    reder json: @post, status: :ok
+  end
+
+  private
+
+  def create_params
+    params.require(:post).permit(:title, :content, :published, :user_id)
+  end
+
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
   end
 end
